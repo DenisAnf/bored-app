8c36f3889fcc88ee815e4159c3a5d6429ae805ee
DenisAnf <ad.vox3@gmail.com>
2023-08-09 03:04:57 +0300

    start js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const body = document.body;
+const questionNode = document.querySelector('.js-question');
+const ideaNode = document.querySelector('.js-idea');
+const buttonGetIdea = document.querySelector('.js-button-go');
+
+const QUESTION_INITION_VALUE = 'Стало скучно?';
+const IDEA_INITION_VALUE = 'Найти, чем заняться?';
+const BACKGROUND_WITH_IDEA = 'linear-gradient(180deg, rgba(0, 176, 28, 0.20) 24.48%, rgba(255, 255, 255, 0.00) 100%)';
+//!добавить эмоджи для вопроса
+const ApiLink = "https://www.boredapi.com/api/activity/";
+
+
+
+body.classList.add('ghghg');
