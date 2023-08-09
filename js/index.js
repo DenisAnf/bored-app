5a78cffcf6a9928ccf1f2ed756022ed5a91a257f
DenisAnf <ad.vox3@gmail.com>
2023-08-09 22:06:38 +0300

    ended js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,60 @@
 const body = document.body;
 const questionNode = document.querySelector('.js-question');
 const ideaNode = document.querySelector('.js-idea');
-const buttonGetIdea = document.querySelector('.js-button-go');
+const buttonShowIdea = document.querySelector('.js-button-go');
 
-const QUESTION_INITION_VALUE = 'Стало скучно?';
+const QUESTION_INITION_VALUE = '🤔 Стало скучно?';
+const ANSWER_INITION_VALUE = 'Ура, теперь не скучно 🔥';
 const IDEA_INITION_VALUE = 'Найти, чем заняться?';
+const IDEA_FOR_LOAD_TIME_VALUE = 'Загрузка...';
+const BACKGROUND_WITHOUT_IDEA = 'linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.20) 47.62%)';
 const BACKGROUND_WITH_IDEA = 'linear-gradient(180deg, rgba(0, 176, 28, 0.20) 24.48%, rgba(255, 255, 255, 0.00) 100%)';
-//!добавить эмоджи для вопроса
 const ApiLink = "https://www.boredapi.com/api/activity/";
 
 
+const init = () => {
+	body.style.background = BACKGROUND_WITHOUT_IDEA;
+	questionNode.textContent = QUESTION_INITION_VALUE;
+	ideaNode.textContent = IDEA_INITION_VALUE;
+};
+init();
 
-body.classList.add('ghghg');
+const disableButtonHandler = () => buttonShowIdea.disabled = true;
+
+const activeButtonHandler = () => buttonShowIdea.disabled = false;
+
+const getIdeaActivityHandlerFromApi = () => {
+	disableButtonHandler();
+	body.style.background = BACKGROUND_WITHOUT_IDEA;
+	ideaNode.textContent = IDEA_FOR_LOAD_TIME_VALUE;
+	
+	fetch(ApiLink)
+		
+		.then(response => {
+			if (!response.ok) {
+				ideaNode.textContent = `Ошибка загруprи из API: ${response.status}`;
+				activeButtonHandler();
+			};
+			return response.json();
+		})
+		
+		.then(data => {
+			const idea = data.activity;
+			if (typeof idea !== 'string') {
+				ideaNode.textContent = 'Почему API вернул не строку...';
+				activeButtonHandler();
+				return;
+			}
+			ideaNode.textContent = idea;
+			questionNode.textContent = ANSWER_INITION_VALUE;
+			body.style.background = BACKGROUND_WITH_IDEA;
+			activeButtonHandler();
+		})
+	
+		.catch(error => {
+			ideaNode.textContent = `Ошибка: ${error.message}`;
+			activeButtonHandler();
+		})
+};
+
+buttonShowIdea.addEventListener('click', getIdeaActivityHandlerFromApi);
